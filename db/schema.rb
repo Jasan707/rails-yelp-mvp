18eea0079bd36ae2a0089b00577cf934b6621dbd
Jasan Sangma <waldura700@gmail.com>
2022-07-13 00:37:16 +0530

    figured out how to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_07_12_183933) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "stars"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
+    t.string "phone_number"
+  end
 
 end
